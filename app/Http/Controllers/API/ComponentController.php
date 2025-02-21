3666bb4e53ed1fc346e6fe67e33dd47049008b2c
levprost <smolev17@gmail.com>
2025-02-21 16:13:21 +0100

    Component controller

--- app/Http/Controllers/API/ComponentController.php
+++ app/Http/Controllers/API/ComponentController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Component;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ComponentController extends Controller
 {
@@ -24,7 +26,40 @@ class ComponentController extends Controller
      */
     public function store(Request $request)
     {
-        
+        $request->validate([
+            'name_component' => 'required|string',
+            'subtitle_component' => 'required|string',
+            'price_component' => 'required|numeric',
+            'description_component' => 'required',
+            'consumption_component' => 'required|integer',
+            'review_component' => 'required',
+            'image_component' => 'required|string',
+            'video_component' => 'required|string',
+            'release_date_component' => 'required|date',
+            'type_component' => 'required|string',
+            'category_id' => 'required|integer',
+            'brand_id' => 'required|integer',
+        ]);
+        Component::create($request->all());
+        return response()->json([
+            'status' => 'Création effectuée avec succès'
+        ]);
+        // Schema::create('components', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name_component');
+        //     $table->string('subtitle_component')->nullable();
+        //     $table->decimal('price_component');
+        //     $table->text('description_component');
+        //     $table->integer('consumption_component');
+        //     $table->text('review_component');
+        //     $table->string('image_component')->nullable();
+        //     $table->string('video_component')->nullable();
+        //     $table->date('release_date_component');
+        //     $table->string('type_component')->nullable();
+        //     $table->foreignId('category_id')->constrained();
+        //     $table->foreignId('brand_id')->constrained();
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -32,7 +67,11 @@ class ComponentController extends Controller
      */
     public function show(Component $component)
     {
-        //
+        $component->load(['brand', 'category']);
+
+        return response()->json([
+            'component' => $component
+        ]);
     }
 
     /**
@@ -40,7 +79,22 @@ class ComponentController extends Controller
      */
     public function update(Request $request, Component $component)
     {
-        //
+        $formFields = $request->validate([
+            'name_component' => 'sometimes|string',
+            'subtitle_component' => 'sometimes|string',
+            'price_component' => 'sometimes|numeric',
+            'description_component' => 'sometimes',
+            'consumption_component' => 'sometimes|integer',
+            'review_component' => 'sometimes',
+            'image_component' => 'sometimes|string',
+            'video_component' => 'sometimes|string',
+            'release_date_component' => 'sometimes|date',
+            'type_component' => 'sometimes|string',
+            'category_id' => 'sometimes|integer',
+            'brand_id' => 'sometimes|integer',
+        ]);
+        $component->update($formFields);
+        return response()->json([$component,'status' => 'Mise à jour effectuée avec succès']);
     }
 
     /**
@@ -48,6 +102,9 @@ class ComponentController extends Controller
      */
     public function destroy(Component $component)
     {
-        //
+        $component->delete();
+        return response()->json([
+            'status' => 'Suppression effectuée avec succès'
+        ]);
     }
 }
